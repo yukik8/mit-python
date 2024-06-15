ac5a772f893e37134bfef3294a7d0a1ea4ad0518
yukik8 <andres8.yuuki@gmail.com>
2024-06-15 12:38:50 +0900

    initial commit

---
+++ 2-4-birthyear.py
@@ -0,0 +1,2 @@
+birthday=input('When is your birthday(mm/dd/yyyy)?\n')
+print('You were born in the year', birthday[6:10])
---
+++ 2-5-while-1.py
@@ -0,0 +1,5 @@
+num_x=int(input('How many times should I print the letter X?　'))
+to_print=''
+while(len(to_print)<abs(num_x)):
+    to_print = to_print + 'X'
+print(to_print)
